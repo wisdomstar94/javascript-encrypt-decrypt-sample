c65a504de1a76abb78c94b65e25180b8920b4092
wisdomstar94 <wisdomstar94@gmail.com>
2022-06-21 09:54:12 +0900

    aes-256-cbc 에서 url encode decode 대응 코드 추가

--- src/aes-256-cbc/aes-256-cbc.ts
+++ src/aes-256-cbc/aes-256-cbc.ts
@@ -11,14 +11,14 @@ class AES256 {
     const ivWA = enc.Utf8.parse(key.substring(0, 16));
 
     const cipher = AES.encrypt(dataWA, keyWA, { iv: ivWA });
-    return cipher.toString();
+    return cipher.ciphertext.toString(enc.Base64url); // url query 파라미터 대응하기 위해 일반 base64 가 아닌 base64url 로 변환
   }
 
   decrypt(encData: string, key: string): string {
     const keyWA = enc.Utf8.parse(key);
     const ivWA = enc.Utf8.parse(key.substring(0, 16));
 
-    const cipher = AES.decrypt(encData, keyWA, { iv: ivWA });
+    const cipher = AES.decrypt(enc.Base64url.parse(encData).toString(enc.Base64), keyWA, { iv: ivWA }); // 해쉬된 값이 url encode 일 수도 있으므로 base64url 로 파싱하고 다시 base64 로 인코딩
     return cipher.toString(enc.Utf8);
   }
 }
