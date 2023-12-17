b8397acb0ae6c7ac965b759029c0be577a9f54b0
wisdomstar94 <wisdomstar94@gmail.com>
2023-12-17 15:15:00 +0900

    AES256 코드에서 crypto-js 종속성 제거 및 내장 crypto 모듈을 사용하는 코드로 수정.

--- src/aes-256-cbc/aes-256-cbc.ts
+++ src/aes-256-cbc/aes-256-cbc.ts
@@ -1,25 +1,26 @@
-import { AES, enc } from 'crypto-js';
+import { scryptSync, randomBytes, createCipheriv, createDecipheriv } from 'crypto';
 
 class AES256 {
   constructor() {
 
   }
 
-  encrypt(data: string, key: string): string {
-    const dataWA = enc.Utf8.parse(data);
-    const keyWA = enc.Utf8.parse(key);
-    const ivWA = enc.Utf8.parse(key.substring(0, 16));
-
-    const cipher = AES.encrypt(dataWA, keyWA, { iv: ivWA });
-    return cipher.ciphertext.toString(enc.Base64url); // url query 파라미터 대응하기 위해 일반 base64 가 아닌 base64url 로 변환
+  encrypt(data: string, encKey: string, salt: string): string {
+    const key = scryptSync(encKey, salt, 32);
+    const iv = randomBytes(16);
+    const cipher = createCipheriv('aes-256-cbc', key, iv); //key는 32바이트, iv는 16바이트
+    let result = cipher.update(data, 'utf8', 'base64');
+    result += cipher.final('base64');
+    return result;
   }
 
-  decrypt(encData: string, key: string): string {
-    const keyWA = enc.Utf8.parse(key);
-    const ivWA = enc.Utf8.parse(key.substring(0, 16));
-
-    const cipher = AES.decrypt(enc.Base64url.parse(encData.replace(/=/gi, '')).toString(enc.Base64), keyWA, { iv: ivWA }); // 해쉬된 값이 url encode 일 수도 있으므로 base64url 로 파싱하고 다시 base64 로 인코딩
-    return cipher.toString(enc.Utf8);
+  decrypt(encData: string, encKey: string, salt: string): string {
+    const key = scryptSync(encKey, salt, 32);
+    const iv = randomBytes(16);
+    const deciper = createDecipheriv('aes-256-cbc', key, iv);
+    let result = deciper.update(encData, 'base64', 'utf8');
+    result += deciper.final('utf8');
+    return result;
   }
 }
 
@@ -29,10 +30,10 @@ function test() {
   const myData = `{"name":"홍길동","age":32}`;
   const myKey = `12345678901234567890123456789012`;
   
-  const encData = aes256.encrypt(myData, myKey);
+  const encData = aes256.encrypt(myData, myKey, '1122');
   console.log('encData', encData);
 
-  const decData = aes256.decrypt(encData, myKey);
+  const decData = aes256.decrypt(encData, myKey, '1122');
   console.log('decData', decData);
 }
 
